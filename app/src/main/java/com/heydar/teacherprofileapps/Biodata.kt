84a6993506a253c1e859200d906dbdd6d47e61a6
heydaristo <heydaristo@gmail.com>
2022-09-07 13:47:45 +0700

    ganti sedikit

--- app/src/main/java/com/heydar/teacherprofileapps/Biodata.kt
+++ app/src/main/java/com/heydar/teacherprofileapps/Biodata.kt
@@ -1,11 +1,13 @@
 package com.heydar.teacherprofileapps
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class Biodata : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_biodata)
+        }
     }
-}
